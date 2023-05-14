6653e5482cd2804b99a05364062beb6229fa1b3d
Tamara Antic <tasaantic00@gmail.com>
2023-05-15 00:59:11 +0200

    search and book

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -61,6 +61,10 @@ function Navbar() {
         <li className="nav-item active">
           <Link className="nav-link" to="/viewAccomodation">View Accommodations</Link>
         </li>)}
+        {loggedUser?.role.toString() == "HOST"  && (
+        <li className="nav-item active">
+          <Link className="nav-link" to="/viewBookings">View Booking Requests</Link>
+        </li>)}
 
       </ul>
       
